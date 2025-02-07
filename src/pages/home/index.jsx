049d65ce4a905e65a9ba8b026b684f45bc8df75e
JMCN <joaomouratocn@gmail.com>
2025-02-06 23:43:12 -0300

    terminado desenho tabela de temperatura

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,29 +4,69 @@ import Logo from '../../assets/logo-horizontal-branco.png'
 function Home() {
   return (
     <div className='container'>
+      <p className='user'>João Mourato</p>
+      <p>Sair</p>
       <h1>Nome da unidade</h1>
       <form >
-        <input type="text" placeholder='Temperatura' />
-        <input type="text" placeholder='Nome' />
+        <p>Geladeira</p>
+        <input type="text" placeholder='Maxíma' />
+        <input type="text" placeholder='Mínima' />
+        <input type="text" placeholder='Atual' />
+        <p>Ambiente</p>
+        <input type="text" placeholder='Maxíma' />
+        <input type="text" placeholder='Mínima' />
+        <input type="text" placeholder='Atual' />
         <button>Registrar</button>
       </form>
       <table border="1">
         <thead>
           <tr>
-            <th colspan="2">Cabeçalho Principal</th>
-            <th>Cabeçalho 2</th>
+            <th rowSpan="2">Data</th>
+            <th colspan="3">Temperatura Geladeira</th>
+            <th colspan="3">Temperatura Ambiente</th>
+            <th rowSpan="2">Usuário</th>
           </tr>
           <tr>
-            <th>Subcabeçalho 1</th>
-            <th>Subcabeçalho 2</th>
-            <th>Subcabeçalho 3</th>
+            <th>Máxima</th>
+            <th>Mínima</th>
+            <th>Atual</th>
+            <th>Máxima</th>
+            <th>Mínima</th>
+            <th>Atual</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>Dados 1.1</td>
-            <td>Dados 1.2</td>
-            <td>Dados 2</td>
+            <td>06/02/2025 - 07:00</td>
+            <td>3.2 °C</td>
+            <td>3.0 °C</td>
+            <td>3.1 °C</td>
+            <td>18.0 °C</td>
+            <td>15.9 °C</td>
+            <td>17 °C</td>
+            <td>João Mourato</td>
+          </tr>
+
+          <tr>
+            <td>06/02/2025 - 12:00</td>
+            <td>3.2 °C</td>
+            <td>3.0 °C</td>
+            <td>3.1 °C</td>
+            <td>18.0 °C</td>
+            <td>15.9 °C</td>
+            <td>17 °C</td>
+            <td>João Mourato</td>
+          </tr>
+
+          <tr>
+            <td>06/02/2025 - 17:00</td>
+            <td>3.2 °C</td>
+            <td>3.0 °C</td>
+            <td>3.1 °C</td>
+            <td>18.0 °C</td>
+            <td>15.9 °C</td>
+            <td>17.0 °C</td>
+            <td>João Mourato</td>
           </tr>
         </tbody>
       </table>
