6e99f4f0089600ee2125faa770cbf767f8be3896
João Mourato <joaomouratocn@gmail.com>
2025-02-11 13:30:19 -0300

    adicionado requisição para colelar unidades

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,9 +1,32 @@
 import './style.css'
+import axios from 'axios';
 import Logo from '../../assets/logo-horizontal-branco.png'
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function Register() {
+  const [options, setOptions] = useState([])
+  const [selectedOptions, setSelectedOption] = useState('')
   const navigate = useNavigate();
+
+
+  const fetchUnits = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/units')
+      setOptions(response.data)
+    } catch (error) {
+      console.log('Erro ao buscar os dados', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchUnits();
+  }, [])
+
+  const handleSelect = (e) => {
+    setSelectedOption(e.target.value)
+  }
 
   const handleRegister = (e) => {
     e.preventDefault();
@@ -18,28 +41,27 @@ function Register() {
         <h2 className='label-register'>Cadastro</h2>
         <form onSubmit={handleRegister} className='form-register'>
           <label className='label-register'>Nome:</label>
-          <input type="text" name='name' placeholder='Insira seu nome' className='input-register' id='input-name-register'/>
+          <input type="text" name='name' placeholder='Insira seu nome' className='input-register' id='input-name-register' />
           <label className='label-register'>Email:</label>
-          <input type="email" name='email' placeholder='Insira seu e-mail' className='input-register' id='input-email-register'/>
+          <input type="email" name='email' placeholder='Insira seu e-mail' className='input-register' id='input-email-register' />
           <label className='label-register'>Senha:</label>
-          <input type="password" name='password' placeholder='Insira uma senha' className='input-register' id='input-password-register'/>
+          <input type="password" name='password' placeholder='Insira uma senha' className='input-register' id='input-password-register' />
           <label className='label-register'>Confirme a senha:</label>
-          <input type="password" name='confirm-password' placeholder='Repita a senha' className='input-register' id='input-confirm-pass-register'/>
+          <input type="password" name='confirm-password' placeholder='Repita a senha' className='input-register' id='input-confirm-pass-register' />
           <label className='label-register'>Selecione unidade:</label>
-          <select name="unit" className='input-register' id='select-unit-register'>
-            <option value="select">Selecione</option>
-            <option value="AV">Água Vermelha</option>
-            <option value="BT">Botafogo</option>
-            <option value="CA">Cidade Aracy</option>
-            <option value="DT">Delta</option>
-            <option value="IM">Itamaraty</option>
-            <option value="JK">Jockey Club</option>
-            <option value="RD">Redenção</option>
-            <option value="SE">Santa Eudoxia</option>
-            <option value="SF">Santa Felicia</option>
-            <option value="SJ">São José</option>
-            <option value="VI">Vila Isabel</option>
-            <option value="ZV">Zavaglia</option>
+          <select
+            name="unit"
+            className='input-register'
+            id='select-unit-register'
+            value={selectedOptions}
+            onChange={handleSelect}>
+            <option value="">Selecione</option>
+            {
+              options.map((option) => (
+                <option key={option.id} value={option.value}>
+                  {option.name}
+                </option>
+              ))}
           </select>
           <button type='submit' className='button-register'>Cadastrar</button>
         </form>
